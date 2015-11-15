4061b09cc84a07c0fe4616a241216a2d2114844b
Emily Horsman <me@emilyhorsman.com>
2015-11-15 12:21:51 -0500

    Add a sidebar widget area.

--- functions.php
+++ functions.php
@@ -1,56 +1,71 @@
 <?php
 
 if ( ! class_exists( 'Timber' ) ) {
-	add_action( 'admin_notices', function() {
-			echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
-		} );
-	return;
+  add_action( 'admin_notices', function() {
+      echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
+    } );
+  return;
 }
 
 Timber::$dirname = array('templates', 'views');
 
 class StarterSite extends TimberSite {
 
-	function __construct() {
-		add_theme_support( 'post-formats' );
-		add_theme_support( 'post-thumbnails' );
-		add_theme_support( 'menus' );
-		add_filter( 'timber_context', array( $this, 'add_to_context' ) );
-		add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
-		add_action( 'init', array( $this, 'register_post_types' ) );
-		add_action( 'init', array( $this, 'register_taxonomies' ) );
-		parent::__construct();
-	}
+  function __construct() {
+    add_theme_support( 'post-formats' );
+    add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'menus' );
+    add_filter( 'timber_context', array( $this, 'add_to_context' ) );
+    add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
+    add_action( 'init', array( $this, 'register_post_types' ) );
+    add_action( 'init', array( $this, 'register_taxonomies' ) );
 
-	function register_post_types() {
-		//this is where you can register custom post types
-	}
+    add_action('widgets_init', array($this, 'sidebar_widgets_init'));
 
-	function register_taxonomies() {
-		//this is where you can register custom taxonomies
-	}
+    parent::__construct();
+  }
 
-	function add_to_context( $context ) {
-		$context['foo'] = 'bar';
-		$context['stuff'] = 'I am a value set in your functions.php file';
-		$context['notes'] = 'These values are available everytime you call Timber::get_context();';
-		$context['menu'] = new TimberMenu();
-		$context['site'] = $this;
-		return $context;
-	}
+  function sidebar_widgets_init() {
+    register_sidebar(array(
+      'name'          => 'Site Sidebar',
+      'id'            => 'site_sidebar',
+      'before_widget' => '<div class="site-sidebar-widget">',
+      'after_widget'  => '</div>',
+      'before_title'  => '<div class="site-sidebar-widget-title">',
+      'after_title'   => '</div>',
+    ));
+  }
 
-	function add_to_twig( $twig ) {
-		/* this is where you can add your own fuctions to twig */
-		$twig->addExtension( new Twig_Extension_StringLoader() );
-		$twig->addFilter( 'myfoo', new Twig_Filter_Function( 'myfoo' ) );
-		return $twig;
-	}
+  function register_post_types() {
+    //this is where you can register custom post types
+  }
+
+  function register_taxonomies() {
+    //this is where you can register custom taxonomies
+  }
+
+  function add_to_context( $context ) {
+    $context['foo'] = 'bar';
+    $context['stuff'] = 'I am a value set in your functions.php file';
+    $context['notes'] = 'These values are available everytime you call Timber::get_context();';
+    $context['menu'] = new TimberMenu();
+    $context['site'] = $this;
+    $context['sidebar'] = Timber::get_widgets('site_sidebar');
+    return $context;
+  }
+
+  function add_to_twig( $twig ) {
+    /* this is where you can add your own fuctions to twig */
+    $twig->addExtension( new Twig_Extension_StringLoader() );
+    $twig->addFilter( 'myfoo', new Twig_Filter_Function( 'myfoo' ) );
+    return $twig;
+  }
 
 }
 
 new StarterSite();
 
 function myfoo( $text ) {
-	$text .= ' bar!';
-	return $text;
+  $text .= ' bar!';
+  return $text;
 }
