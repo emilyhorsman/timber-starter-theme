10d316cfdea7df06493b15a3e655e06faa9ca70f
Emily Horsman <me@emilyhorsman.com>
2015-11-15 12:22:43 -0500

    Remove some starter things

--- functions.php
+++ functions.php
@@ -36,18 +36,10 @@ class StarterSite extends TimberSite {
     ));
   }
 
-  function register_post_types() {
-    //this is where you can register custom post types
-  }
-
-  function register_taxonomies() {
-    //this is where you can register custom taxonomies
-  }
+  function register_post_types() { }
+  function register_taxonomies() { }
 
   function add_to_context( $context ) {
-    $context['foo'] = 'bar';
-    $context['stuff'] = 'I am a value set in your functions.php file';
-    $context['notes'] = 'These values are available everytime you call Timber::get_context();';
     $context['menu'] = new TimberMenu();
     $context['site'] = $this;
     $context['sidebar'] = Timber::get_widgets('site_sidebar');
@@ -55,17 +47,10 @@ class StarterSite extends TimberSite {
   }
 
   function add_to_twig( $twig ) {
-    /* this is where you can add your own fuctions to twig */
     $twig->addExtension( new Twig_Extension_StringLoader() );
-    $twig->addFilter( 'myfoo', new Twig_Filter_Function( 'myfoo' ) );
     return $twig;
   }
 
 }
 
 new StarterSite();
-
-function myfoo( $text ) {
-  $text .= ' bar!';
-  return $text;
-}
