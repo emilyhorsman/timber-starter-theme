d3727a3e03c17500150fcedfa1c7a4401c32c80b
Emily Horsman <me@emilyhorsman.com>
2015-12-08 13:27:44 -0500

    Switched tabs to spaces from the starter theme. Added a .editorconfig file.

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
  */
 
 if ( ! class_exists( 'Timber' ) ) {
-	echo 'Timber not activated. Make sure you activate the plugin in <a href="/wp-admin/plugins.php#timber">/wp-admin/plugins.php</a>';
-	return;
+  echo 'Timber not activated. Make sure you activate the plugin in <a href="/wp-admin/plugins.php#timber">/wp-admin/plugins.php</a>';
+  return;
 }
 $context               = Timber::get_context();
 $context['posts']      = Timber::get_posts();
@@ -23,6 +23,6 @@ $context['pagination'] = Timber::get_pagination();
 
 $templates = array( 'index.twig' );
 if ( is_home() ) {
-	array_unshift( $templates, 'home.twig' );
+  array_unshift( $templates, 'home.twig' );
 }
 Timber::render( $templates, $context );
